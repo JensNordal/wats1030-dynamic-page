a1c596a62bc1e805e17a994df62d93648b350e06
JensNordal <jensnordal@gmail.com>
2016-02-14 17:18:32 -0800

    Commit config.php changes - api, page title and search tags

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = '05d763f96cb4d88671e0f75206239664'; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'Guitars and Pianos'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'guitars,pianos'; // This must be a comma-separated list
 
 ?>
